7d2ff1a827668f5eac82fc542d43780826cea91f
Jacob M. Slone <jslone1993@gmail.com>
2015-03-01 16:56:17 -0500

    triggers can be touch activated or button activated, tweaking gear rotation

--- Assets/Scripts/Triggers/Trigger.cs
+++ Assets/Scripts/Triggers/Trigger.cs
@@ -2,5 +2,15 @@
 using System.Collections;
 
 public abstract class Trigger : MonoBehaviour {
+	public bool TriggerOnEnter;
+	public bool TriggerOnExit;
 	public abstract void OnTrigger();
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if(TriggerOnEnter) OnTrigger();
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		if(TriggerOnExit) OnTrigger();
+	}
 }
