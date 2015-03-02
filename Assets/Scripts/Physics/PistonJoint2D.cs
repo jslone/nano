68712760ed44e1b5b407532b3674f665c0902d53
Jacob M. Slone <jslone1993@gmail.com>
2015-03-02 11:48:07 -0500

    pistons

---
+++ Assets/Scripts/Physics/PistonJoint2D.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistonJoint2D : MonoBehaviour {
+	public bool useMotor;
+	public float Speed;
+	public Vector3 Offset;
+	
+	private Vector3 pos;
+	private float time = 0;
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(useMotor) {
+			time += Time.deltaTime;
+			transform.position = pos + Offset * Mathf.PingPong(Speed * time, 1.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/Triggers/PistonTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistonTrigger : Trigger {
+	public PistonJoint2D Piston;
+	public bool isOscillating
+	{
+		get { return Piston.useMotor; }
+		set { Piston.useMotor = value; }
+	}
+	
+	public override void OnTrigger ()
+	{
+		isOscillating = !isOscillating;
+	}
+}
