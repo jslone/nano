3e04967dec1b958867ca89fd1eb336b808bc0972
Jacob M. Slone <jslone1993@gmail.com>
2015-03-18 22:19:30 -0400

    lava

---
+++ Assets/Scripts/Player/Lava/LavaExpel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaExpel : MonoBehaviour {
+	public float Speed;
+
+	private PlayerController player;
+	new private Rigidbody2D rigidbody2D;
+
+	private Vector3 lastSafe;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<PlayerController>();
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.isGrounded) {
+			lastSafe = transform.position;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.collider.tag == "Lava") {
+			Vector3 dir = lastSafe - transform.position;
+			rigidbody2D.AddForce(Speed/2 * (dir.normalized + Vector3.up));
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Lava/LavaPico.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaPico : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.collider.tag == "Lava") {
+			FindObjectOfType<TogglePlayer>().ZoomOut();
+		}
+	}
+}
