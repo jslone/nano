eb29bb9ab964f65be1d39cadaf9d1b0b85714c0c
Jacob M. Slone <jslone1993@gmail.com>
2015-03-20 01:30:38 -0400

    better slider code, load level stuff

---
+++ Assets/Scripts/Physics/DirectionalSliderJoint2D.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionalSliderJoint2D : MonoBehaviour {
+	public SliderJoint2D joint;
+	public bool useMotor
+	{
+		get
+		{
+			return neg ^ joint.motor.motorSpeed < 0;
+		}
+		set
+		{
+			if(value != useMotor) {
+				JointMotor2D m = joint.motor;
+				m.motorSpeed *= -1;
+				joint.motor = m;
+			}
+		}
+	}
+	bool neg;
+
+	// Use this for initialization
+	void Start () {
+		neg = joint.motor.motorSpeed < 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Utilities/LoadLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Collider2D))]
+public class LoadLevel : MonoBehaviour {
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Application.LoadLevelAdditive(level);
+		Destroy(this);
+	}
+}
