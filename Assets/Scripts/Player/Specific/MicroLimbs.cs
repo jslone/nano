93698c73efee54eef702f7c5316122d494e9a2dc
Jacob M. Slone <jslone1993@gmail.com>
2015-03-20 05:28:34 -0400

    fixing spawner

--- Assets/Scripts/Player/Specific/MicroLimbs.cs
+++ Assets/Scripts/Player/Specific/MicroLimbs.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class MicroLimbs : MonoBehaviour {
 	public static int Limbs = 0;
+	public static int ActiveLimbs = 0;
 
 	private Animator animator;
+	private PlayerController player;
+
 	// Use this for initialization
 	void Awake () {
 		animator = GetComponent<Animator>();
+		player = GetComponent<PlayerController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		animator.SetInteger("limbs", Limbs);
+		player.canMove = ActiveLimbs == Limbs;
 	}
 }
