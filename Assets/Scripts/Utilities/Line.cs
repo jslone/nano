1225b6785e57ddd7c1182762aed0c5d50b4cffea
Jacob M. Slone <jslone1993@gmail.com>
2015-03-19 23:24:47 -0400

    ROPE\!\!\!

---
+++ Assets/Scripts/Utilities/Line.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(LineRenderer))]
+public class Line : MonoBehaviour {
+	public Transform[] links;
+	private LineRenderer lrend;
+
+	// Use this for initialization
+	void Start () {
+		lrend = GetComponent<LineRenderer> ();
+		lrend.SetWidth (1.0f, 1.0f);
+		lrend.SetVertexCount (links.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < links.Length; i++) {
+			lrend.SetPosition(i,links[i].localPosition);
+		}
+	}
+}
