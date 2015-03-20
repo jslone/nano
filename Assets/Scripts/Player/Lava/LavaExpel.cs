b4ccd63496a605b56a1bd9e231d008b9fa1a9562
Jacob M. Slone <jslone1993@gmail.com>
2015-03-20 08:01:02 -0400

    better lava bounce

--- Assets/Scripts/Player/Lava/LavaExpel.cs
+++ Assets/Scripts/Player/Lava/LavaExpel.cs
@@ -2,17 +2,24 @@
 using System.Collections;
 
 public class LavaExpel : MonoBehaviour {
-	public float Speed;
+	public float T = 1;
 
 	private PlayerController player;
 	new private Rigidbody2D rigidbody2D;
+	new private Collider2D collider2D;
+	private float linearDrag;
 
 	private Vector3 lastSafe;
+	private Vector2 v;
+	private Vector2 g;
+	private bool Controlling = false;
 
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<PlayerController>();
 		rigidbody2D = GetComponent<Rigidbody2D>();
+		collider2D = GetComponent<Collider2D>();
+		g = 0.2f * rigidbody2D.gravityScale * Physics2D.gravity;
 	}
 	
 	// Update is called once per frame
@@ -22,10 +29,39 @@ public class LavaExpel : MonoBehaviour {
 		}
 	}
 
+	void FixedUpdate() {
+		if(Controlling) {
+			transform.position += (Vector3)v * Time.fixedDeltaTime;
+			v += g * Time.fixedDeltaTime;
+		}
+	}
+
 	void OnCollisionEnter2D(Collision2D col) {
 		if(col.collider.tag == "Lava") {
-			Vector3 dir = lastSafe - transform.position;
-			rigidbody2D.AddForce(Speed/2 * (dir.normalized + Vector3.up));
+			Controlling = true;
+
+			Vector3 target = lastSafe - transform.position;
+			
+			v = Vector2.zero;
+			v.x = target.x / T;
+			v.y = (target.y - (g.y * T * T) / 2) / T;
+
+			if(v.x < 1f && v.x > -1f) {
+				v.x = 1f * Mathf.Sign(v.x);
+			}
+
+			player.canMove = false;
+			rigidbody2D.isKinematic = true;
+			collider2D.enabled = false;
+
+			Invoke("Restore",T);
 		}
+	}
+
+	void Restore() {
+		Controlling = false;
+		player.canMove = true;
+		rigidbody2D.isKinematic = false;
+		collider2D.enabled = true;
 	}
 }
