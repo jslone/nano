76bfc74db50b264b2c19016a90a0656d35f9b477
Jacob M. Slone <jslone1993@gmail.com>
2015-03-20 02:37:50 -0400

    micro limbs

---
+++ Assets/Scripts/Player/Specific/MicroLimbs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicroLimbs : MonoBehaviour {
+	public int Limbs
+	{
+		get { return _limbs; }
+		set
+		{
+			_limbs = value;
+			animator.SetInteger("limbs",value);
+		}
+	}
+
+	private int _limbs = 0;
+	private Animator animator;
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
