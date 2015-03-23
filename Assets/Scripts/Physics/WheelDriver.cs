c92efdc3817172b0e355cfe2c55e1262ca73e944
Jacob M. Slone <jslone1993@gmail.com>
2015-03-23 13:47:50 -0400

    wheel driver, fixed collider

---
+++ Assets/Scripts/Physics/WheelDriver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelDriver : MonoBehaviour {
+	public Rigidbody2D driver;
+	public DirectionalSliderJoint2D reciever;
+	public float Scale;
+	private float Offset;
+
+	// Use this for initialization
+	void Start () {
+		Offset = reciever.joint.motor.motorSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		JointMotor2D motor = reciever.joint.motor;
+		motor.motorSpeed = Scale * driver.angularVelocity + Offset;
+		reciever.joint.motor = motor;
+	}
+}
