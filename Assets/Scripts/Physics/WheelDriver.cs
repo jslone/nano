9a5de2e7ec75ea08138d61833fc34c7789cf7948
Jacob M. Slone <jslone1993@gmail.com>
2015-03-23 22:51:34 -0400

    multiple door controls

--- Assets/Scripts/Physics/WheelDriver.cs
+++ Assets/Scripts/Physics/WheelDriver.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class WheelDriver : MonoBehaviour {
 	public Rigidbody2D driver;
-	public DirectionalSliderJoint2D reciever;
+	public DirectionalSliderJoint2D[] reciever;
 	public float Scale;
-	private float Offset;
+	private float[] Offset;
 
 	// Use this for initialization
 	void Start () {
-		Offset = reciever.joint.motor.motorSpeed;
+		Offset = new float[reciever.Length];
+		for(int i = 0; i < reciever.Length; i++) {
+			Offset[i] = reciever[i].joint.motor.motorSpeed;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		JointMotor2D motor = reciever.joint.motor;
-		motor.motorSpeed = Scale * driver.angularVelocity + Offset;
-		reciever.joint.motor = motor;
+		for(int i = 0; i < reciever.Length; i++) {
+			JointMotor2D motor = reciever[i].joint.motor;
+			motor.motorSpeed = Scale * driver.angularVelocity + Offset[i];
+			reciever[i].joint.motor = motor;
+		}
 	}
 }
