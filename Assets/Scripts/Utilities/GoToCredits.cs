e226af9a4b137a91dc45961ae02153efe468ee62
Jacob M. Slone <jslone1993@gmail.com>
2015-03-27 09:35:24 -0400

    go to credits after 15 sec on endgame, fixed audio for title and credits

---
+++ Assets/Scripts/Utilities/GoToCredits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToCredits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("LoadCredits",15);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LoadCredits() {
+		Application.LoadLevel("credits");
+	}
+}
