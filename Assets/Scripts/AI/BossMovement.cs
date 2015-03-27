87f94144cffc9f0cbac07f70abe21140418c2a67
Jacob M. Slone <jslone1993@gmail.com>
2015-03-27 03:00:06 -0400

    boss will get you

---
+++ Assets/Scripts/AI/BossMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMovement : MonoBehaviour {
+	public float Delay;
+	new private SliderJoint2D slider;
+	// Use this for initialization
+	void Start () {
+		Invoke ("BeginMove", Delay);
+	}
+	
+	void BeginMove() {
+		GetComponent<SliderJoint2D>().useMotor = true;
+	}
+}
