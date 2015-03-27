7af7735a7f50a1efd6b735c00c87c30b9dd850a2
Ivan Wang <icw@cmu.edu>
2015-03-27 08:05:31 -0400

    basic boss defeat script

---
+++ Assets/Scripts/Stuff/DefeatVirus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefeatVirus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameState.level == 8) {
+			GameState.SetLevel(9);
+			// TODO: make virus sink
+			GetComponent<SliderJoint2D>().enabled = false;
+			Invoke("EndGame", 4f);
+		}
+	}
+
+	void EndGame () {
+		Application.LoadLevel("microWake");
+	}
+
+}
