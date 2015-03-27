298813c451b0974ee5b83ab31b294415aed32bd1
Diana Zhu <dianaz@andrew.cmu.edu>
2015-03-27 04:01:52 -0400

    added buttons to menu

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ButtonAction {
+	PLAY,
+	CREDITS,
+	QUIT
+}
+
+public class UIButton : MonoBehaviour {
+	public ButtonAction action;
+	
+	public void OnClick() {
+		switch(action) {
+			case ButtonAction.PLAY:
+				Application.LoadLevel(0);	// game scene
+				break;
+			case ButtonAction.CREDITS:
+				Application.LoadLevel(6);	// credits scene
+				break;
+			case ButtonAction.QUIT:
+				Application.Quit();
+				break;
+		}
+	}
+}
