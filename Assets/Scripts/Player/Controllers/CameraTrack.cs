eaf64e03f1e9d48a1fd682eea4653c0846debf6c
Ivan Wang <icw@cmu.edu>
2016-01-21 16:52:11 -0500

    update animations and improve camera tracking in cutscene

---
+++ Assets/Scripts/Player/Controllers/CameraTrack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrack : MonoBehaviour {
+	public CameraData camData;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("SetTarget", 0.1f); // hacky wait so it's set after player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SetTarget() {
+		FindObjectOfType<CameraController>().playerCamera = camData;
+	}
+
+	void ResetTarget() {
+		CameraController camController = FindObjectOfType<CameraController>();
+		camController.playerCamera = camController._lastPlayerCamera;
+	}
+}
